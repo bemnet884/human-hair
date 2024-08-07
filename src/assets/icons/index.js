72bdc943a9387b823c187462a7a9ebfb076fcc10
bemnet884 <155265449+bemnet884@users.noreply.github.com>
2024-08-06 20:38:34 -0700

    c assest

---
+++ src/assets/icons/index.js
@@ -0,0 +1,30 @@
+import arrowRight from './arrow-right.svg'
+import chevronLeft from './chevron-left.svg'
+import cheveronRight from './chevron-right.svg'
+import copyrightSign from './copyright-sign.svg'
+import facebook from './facebook.svg'
+import hamburger from './hamburger.png'
+import instagram from './instagram.svg'
+import support from './support.svg'
+import shieldTick from './shield-tick.svg'
+import star from './star.svg'
+import truckFast from './truck-fast.svg'
+import twitter from './twitter.svg'
+import shopLogo from './shopLogo.png'
+import logo2 from './logo2.jpeg'
+export {
+    arrowRight,
+    cheveronRight,
+    chevronLeft,
+    copyrightSign,
+    facebook,
+    hamburger,
+    instagram,
+    support,
+    shieldTick,
+    star,
+    truckFast,
+    twitter,
+    shopLogo,
+    logo2
+}
