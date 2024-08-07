4f801860ccd8f910dc16080de6c728646eac827b
bemnet884 <155265449+bemnet884@users.noreply.github.com>
2024-08-06 20:39:11 -0700

    c images

---
+++ src/assets/images/index.js
@@ -0,0 +1,46 @@
+import hairBundle1 from './hairBundle1.jpg';
+import hairBundle2 from './Hoooo.jpg';
+import hairBundle3 from './Hiii.jpg'
+
+import customer1 from './customer1.webp';
+import customer2 from './customer2.jpeg';
+
+import footerLogo from './hair-2.webp';
+import headerLogo from './hair-3.webp';
+
+import offer from './offer.webp';
+
+import hairProduct1 from './hairProduct1.jpeg';
+import hairProduct2 from './hair-1.jpg';
+import hairProduct3 from './hairProduct2.jpg';
+import hairProduct4 from './hairProduct3.jpeg';
+import hairProduct5 from './hairProduct4.jpeg';
+
+import thumbnailBackground from './thumbnailBackground.png';
+import thumbnailHair1 from './thumbnail1.webp';
+import thumbnailHair2 from './thumbnail2.webp';
+import thumbnailHair3 from './thumbnail3.webp';
+
+export {
+    hairBundle1,
+    hairBundle2,
+    hairBundle3,
+
+    customer1,
+    customer2,
+
+    footerLogo,
+    headerLogo,
+
+    offer,
+    hairProduct1,
+    hairProduct2,
+    hairProduct3,
+    hairProduct4,
+    hairProduct5,
+
+    thumbnailBackground,
+    thumbnailHair1,
+    thumbnailHair2,
+    thumbnailHair3
+};
